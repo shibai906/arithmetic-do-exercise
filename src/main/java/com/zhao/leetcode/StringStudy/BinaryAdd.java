8f4d19d175d9651a16d75e1c01c0dc69b64e2f3e
zhaohuan <zh155906@163.com>
2019-07-31 21:58:03 +0800

    删除二进制中，从前往后走的思路，因为有111这样的，就特别复杂

--- src/main/java/com/zhao/leetcode/StringStudy/BinaryAdd.java
+++ src/main/java/com/zhao/leetcode/StringStudy/BinaryAdd.java
@@ -11,40 +11,6 @@ public class BinaryAdd {
         System.out.println(addBinary("1010","1011"));
     }
 
-    public static String elseAddBinary(String a, String b) {
-
-        StringBuilder sb = new StringBuilder();
-        int i = 0 , j = b.length() - 1,num = 0;
-        while (i >= 0 || j >= 0) {
-            int ai = 48 ;
-            int bj = 48 ;
-            if(i >= 0) {
-                ai = a.charAt(i--);
-            }
-            if(j >= 0) {
-                bj = b.charAt(j--);
-            }
-            int add = ai + bj + num - 48;
-            if(add < 50) {
-                sb.append((char)add);
-                num = 0;
-            }
-            if(add == 50) {
-                sb.append('0');
-                num = 1;
-            }
-            if(add == 51) {
-                sb.append('1');
-                num = 1;
-            }
-        }
-        if(num == 1) {
-            sb.append('1');
-        }
-        return sb.toString();
-
-    }
-
     public static String addBinary(String a, String b) {
 
         StringBuilder sb = new StringBuilder();

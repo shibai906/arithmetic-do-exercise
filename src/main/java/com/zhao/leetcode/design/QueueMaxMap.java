e44ede71e485c66aa4450beba3c9e1c969c133d6
zhaohuan1 <zhaohuan1@xiaomi.com>
2019-10-29 16:02:04 +0800

    添加maxMap类

--- src/main/java/com/zhao/leetcode/design/QueueMaxMap.java
+++ src/main/java/com/zhao/leetcode/design/QueueMaxMap.java
@@ -2,8 +2,43 @@ package com.zhao.leetcode.design;
 
 import com.zhao.leetcode.linked.Node;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class QueueMaxMap {
 
-//    Node head
+    Node head ;
+    Node opeHead ;
+
+    TreeMap<Integer,Integer> map = new TreeMap<>();
+
+    public void push(int value) {
+        if(head == null) {
+            head = new Node(value);
+            opeHead = head;
+        } else {
+            opeHead.next = new Node(value);
+        }
+        map.put(value,map.getOrDefault(value,0) + 1);
+    }
+
+    public int pop() {
+        if(head == null) {
+            throw new RuntimeException("此队列中无元素");
+        }
+        int value = head.val;
+        int num = map.get(value);
+        num = num == 0 ? map.remove(value) : map.put(value,num - 1);
+        return value;
+    }
+
+    public int getMax() {
+        if(head == null) {
+            throw new RuntimeException("此队列中无元素");
+        }
+        return map.lastKey();
+    }
+
+
 
 }

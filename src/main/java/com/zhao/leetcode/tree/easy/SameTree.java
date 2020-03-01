d2592d8d5cdc01acb885e1a94f63917e77671214
zh155906 <zh155906@163.com>
2020-03-01 23:36:37 +0800

    添加注释

--- src/main/java/com/zhao/leetcode/tree/easy/SameTree.java
+++ src/main/java/com/zhao/leetcode/tree/easy/SameTree.java
@@ -7,6 +7,10 @@ import com.zhao.leetcode.tree.TreeNode;
  * @description
  * @author: zhaohuan
  * @create: 2020-03-01 18:24
+ * 地址： https://leetcode-cn.com/problems/same-tree/submissions/
+ * 题目： 相同的树
+ * 给定两个二叉树，编写一个函数来检验它们是否相同。
+ * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
  **/
 public class SameTree {
 

1996300742259660e6a30abbe50d307807a024df
zh155906 <zh155906@163.com>
2020-11-01 09:33:05 +0800

    jdk中常用的设计模式

---
+++ src/main/java/com/zhao/design/mode/framework/calendar/Client.java
@@ -0,0 +1,18 @@
+package com.zhao.design.mode.framework.calendar;
+
+import java.util.Calendar;
+
+/**
+ * @program: arithmetic-do-exercise
+ * @author: zhaohuan
+ * @create: 2020-10-25 23:42
+ * @description
+ * 此类使用了工厂模式和建造者设计模式
+ **/
+public class Client {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/com/zhao/design/mode/framework/collections/Client.java
@@ -0,0 +1,22 @@
+package com.zhao.design.mode.framework.collections;
+
+import com.sun.org.apache.xpath.internal.operations.String;
+
+import java.util.Collections;
+
+/**
+ * @program: arithmetic-do-exercise
+ * @author: zhaohuan
+ * @create: 2020-10-26 00:04
+ * @description 使用的设计模式为装饰者设计模式：如UnmodifiableCollection，虽然对一些方法做了抛出异常功能，但也是对功能的一种增强
+ *  也使用到了适配器设计模式： Enumeration 来适配老的遍历容器。
+ **/
+public class Client {
+
+    public static void main(String[] args) {
+//        Collections
+        StringBuilder sb = new StringBuilder();
+    }
+
+}
+

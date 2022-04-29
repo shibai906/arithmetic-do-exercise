868e6246ccea2e0474cd8fdd5c2bf0a0e6e9d3df
zh155906 <zh155906@163.com>
2022-04-30 01:56:07 +0800

    添加贪心算法题

--- src/main/java/com/zhao/leetcode/arr/medium/NoRepeat.java
+++ src/main/java/com/zhao/leetcode/arr/medium/NoRepeat.java
@@ -1,21 +1,21 @@
-package com.zhao;
+package com.zhao.leetcode.arr.medium;
 
 import java.util.Arrays;
-import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @program: arithmetic-do-exercise
  * @author: zhaohuan
- * @create: 2021-09-08 23:46
+ * @create: 2022-04-30 01:54
  * @description
+ * 地址：https://leetcode-cn.com/problems/non-overlapping-intervals/
+ * 题目：无重叠区间
+ * 描述：给定一个区间的集合 intervals ，其中 intervals[i] = [starti, endi] 。返回 需要移除区间的最小数量，使剩余区间互不重叠 。
+ *
  **/
-public class TestAA {
-    public String s = "a";
-    public static void main(String[] args) throws InterruptedException {
-        eraseOverlapIntervals(new int[][]{{1,2},{2,3},{3,4},{1,3}});
-    }
+public class NoRepeat {
 
-    public static int eraseOverlapIntervals(int[][] intervals) {
+    // 贪心算法，右边需要排序
+    public int eraseOverlapIntervals(int[][] intervals) {
         Arrays.sort(intervals, (int[] a1, int[] a2) ->{
             return a1[1] - a2[1];
         });
@@ -29,4 +29,5 @@ public class TestAA {
         }
         return result;
     }
+
 }
